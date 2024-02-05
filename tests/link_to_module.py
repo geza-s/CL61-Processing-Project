bf0674ceb4660437013c2c93ad8752d86e1c4bb4
G.Soldati <geza.soldati@epfl.ch>
2024-02-05 11:13:25 +0100

    restructuring files

---
+++ tests/link_to_module.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+# Adding the path to the module
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Import the module
+import CL61_module
